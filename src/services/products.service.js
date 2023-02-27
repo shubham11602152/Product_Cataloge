692e6cb24eae986f33d698e93d04b4e72707d73d
shubham11602152 <shubhammehta.333@gmail.com>
2023-02-27 12:36:37 +0530

    [Refactored] : Implemented error handling for getProducts service file

--- src/services/products.service.js
+++ src/services/products.service.js
@@ -1,11 +1,20 @@
 import { toProducts } from "../mappers/products-mapper";
 
 const getProducts = async () => {
-  const data = await fetch("https://fakestoreapi.com/products").then((res) =>
-    res.json()
-  );
+  try {
+    const data = await fetch("https://fakestoreapi.com/products").then(
+      (res) => {
+        if (res.status >= 400) {
+          throw new Error("Server responded with error!");
+        }
+        return res.json();
+      }
+    );
 
-  return toProducts(data);
+    return toProducts(data);
+  } catch (e) {
+    console.error(e);
+  }
 };
 
 export { getProducts };
